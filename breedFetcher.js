dc27e60f487796f35298340ef4cc2ad3fd605deb
Elshan Magsudov <elio@jyanik.com>
2022-11-20 00:08:18 -0500

    refactored and added test files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,31 @@
 const request = require("request");
-const fs = require("fs");
-const breedRequest = require("./breedRequest");
 
-const breedFetcher = function (input) {
-  input = process.argv[2];
-  console.log(input);
-  url = "https://api.thecatappi.com/v1/breeds/search?q=" + input;
-  console.log(url);
+const fetchBreedDescription = function (breedName, callback) {
+  // breedName = process.argv[2];
+  let url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
 
   request(url, (error, response, body) => {
-    if (error != null) {
-      console.log("error:", error); // Print the error if one occurred
+    if (error !== null) {
+      callback("error: " + error, null);
+      //     console.log("error:", error); // Print the error if one occurred
     }
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+    //   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
 
     if (body.length < 3) {
-      console.log("requested breed (" + input + ") is not found");
+      //     console.log("requested breed (" + breedName + ") is not found");
+      //     return;
+      callback("requested breed (" + breedName + ") is not found", null);
       return;
     }
     const obj = JSON.parse(body);
-    // console.log(typeof obj);
-    console.log(
-      "Desccription of the 1st element on obj array :",
-      obj[0].description
-    );
+    //   // console.log(typeof obj);
+    //   console.log(
+    //     "Desccription of the 1st element on obj array :",
+    //     obj[0].description
+    //   );
+    callback(null, obj[0].description);
+    // });
   });
 };
 
-breedFetcher();
+module.exports = { fetchBreedDescription };
